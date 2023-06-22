4c24936a6eeb357d10c831e658b6fea1cd5d113b
almalmalm <almostkalinin@gmail.com>
2023-06-22 15:23:04 +0400

    FIX: button border

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 const Button: React.FC<ButtonProps> = ({ text }) => {
   return (
     <button
-      className="absolute bottom-8 left-0 right-0 mr-auto ml-auto border text-[#fbfcff] w-64 border-2 rounded h-14 font-semibold
+      className="absolute bottom-8 left-0 right-0 mr-auto ml-auto text-[#fbfcff] w-64 border-2 rounded h-14 font-semibold
   mobile-md:w-80 mobile-lg:w-96 tablet:w-32 tablet:ml-0 tablet:left-8"
     >
       {text}
